6a6b6dfd767f85abf41c23eaa3dd8c6393b908cd
SH384090 <shubham.chaudhary3@wipro.com>
2018-09-19 17:52:28 +0530

    text added

--- FileHandling/server.js
+++ FileHandling/server.js
@@ -1,9 +1,20 @@
 var http = require('http');//this is a simple file handling example
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
+  fs.rename('index.html','index1.html',(err)=>{
+if (err) {
+ throw err;
+};
+console.log("renamed");
+  });
+
+  fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+  fs.readFile('index1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
-}).listen(8000);
+}).listen(8000);// renaming a file example
