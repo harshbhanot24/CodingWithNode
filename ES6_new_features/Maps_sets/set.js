663278bad44338ca2ad47b4a4169a8a10107d173
SH384090 <shubham.chaudhary3@wipro.com>
2018-09-21 12:25:14 +0530

    set

--- ES6_new_features/Maps_sets/set.js
+++ ES6_new_features/Maps_sets/set.js
@@ -20,4 +20,10 @@
  for(let [key,value] of setObj.entries()){
      console.log("key is"+key+" value is "+ value);
  }//here we used concept of destructuring ie breaking enteries to key and value 2 variables
- 
+ //in set keys and value are same
+
+ //converting array of repeating no to new array via set
+ var arr=[1,22,22,1,3,3,4];
+ var SetR=new Set(arr);
+ var noDuplicateArray=new Array(...SetR);
+ console.log(JSON.stringify(noDuplicateArray));
